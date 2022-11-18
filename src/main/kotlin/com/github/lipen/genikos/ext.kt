c836741c2a4a1540ac9956856798c8f41d6d2980
Konstantin Chukharev <lipen00@gmail.com>
2022-11-19 00:37:57 +0400

    Add `plus` implementation

--- src/main/kotlin/com/github/lipen/genikos/ext.kt
+++ src/main/kotlin/com/github/lipen/genikos/ext.kt
@@ -11,3 +11,24 @@ operator fun <T> GenericArray<out T>.component2(): T = get(1)
 operator fun <T> GenericArray<out T>.component3(): T = get(2)
 operator fun <T> GenericArray<out T>.component4(): T = get(3)
 operator fun <T> GenericArray<out T>.component5(): T = get(4)
+
+operator fun <T> GenericArray<T>.plus(element: T): GenericArray<T> {
+    val result = ArrayList<T>(this.size + 1)
+    result.addAll(this)
+    result.add(element)
+    return GenericArray.from(result)
+}
+
+operator fun <T> GenericArray<T>.plus(elements: Iterable<T>): GenericArray<T> {
+    return if (elements is Collection) {
+        val result = ArrayList<T>(this.size + elements.size)
+        result.addAll(this)
+        result.addAll(elements)
+        GenericArray.from(result)
+    } else {
+        val result = ArrayList<T>(this.size)
+        result.addAll(this)
+        result.addAll(elements)
+        GenericArray.from(result)
+    }
+}
